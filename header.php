c642330ae163bba13d5e9d8a8be1af6be5479816
FortuneMatthew <matthewwoolley31@gmail.com>
2015-06-03 22:17:12 +0100

    Base Files

---
+++ header.php
@@ -0,0 +1,44 @@
+<div class="content">
+	<h1>Fortune Studios</h1>
+	<nav>
+		<ul>
+			<li><a href="">HOME</a></li><!--
+	 --><li><a href="">ABOUT US</a>
+				<ul>
+					<li><a href="">Contact Us</a></li>
+					<li><a href="">Open Source</a></li>
+				</ul>
+			</li><!--
+	 --><li><a href="">SERVICES</a>
+				<ul>
+					<li><a href="">Web Design</a></li>
+				</ul>
+			</li><!--
+	 --><li><a href="">LEARN</a>
+				<ul>
+					<li>
+						<a href="">Unreal Engine 4</a>
+						<ul>
+							<li><a href="">Programming</a>
+						</ul>
+					</li>
+					<li>
+						<a href="">Unity</a>
+						<ul>
+							<li><a href="">Programming</a>
+						</ul>
+					</li>
+					<li>
+						<a href="">Website Design</a>
+						<ul>
+							<li><a href="">HTML</a>
+							<li><a href="">CSS</a>
+							<li><a href="">JavaScript</a>
+							<li><a href="">PHP</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li> 
+		</ul>
+	</nav>
+</div>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords"  content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="__CSS/__MAIN.css" />
+	<link rel="stylesheet" href="__CSS/__MONITOR.css" media="screen and (min-width: 1024px)" />
+	<link rel="stylesheet" href="__CSS/NAV/NAV_MAIN.css"/>
+	<link rel="stylesheet" href="__CSS/NAV/NAV_MONITOR.css" media="screen and (min-width: 1024px)" />
+	<title>Home | Fortune Studios</title>
+</head>
+<body>
+	<header>
+		<?php require "header.php"; ?>
+	</header>
+	<article>
+		<section class="content">
+			<h1>Home</h1>
+			<hr />
+			<h2>Welcome</h2>
+			<p></p>
+		</section>
+	</article>
+</body>
+</html>
